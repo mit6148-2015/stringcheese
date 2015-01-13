d0e8bf582664b2446f3bcfb4f55304ef119d4c72
gracie <gfang@wellesley.edu>
2015-01-13 13:59:18 -0500

    router stuff in progress

--- globaltrends/globaltrends.js
+++ globaltrends/globaltrends.js
@@ -2,6 +2,11 @@ if (Meteor.isClient) {
   Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+  Template.Local.events({
+  'click #clickme': function() {
+    Router.go('/local');
+  }
+});
 }
 
 if (Meteor.isServer) {
@@ -9,3 +14,21 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('/local', function () {
+  this.render('Local');
+});
+
+Router.route('/selected', function () {
+  this.render('Selected');
+});
+
+Router.route('/world', function () {
+  this.render('World');
+});
