bac034e988ad7d22e7b1c22641510aefe92babf2
Carissa Sun <carissa.msun@gmail.com>
2015-01-15 21:15:39 -0500

    made home page

--- globaltrendsv1/globaltrendsv1.js
+++ globaltrendsv1/globaltrendsv1.js
@@ -34,7 +34,8 @@ if (Meteor.isServer) {
 
 Router.map(function(){
   this.route('map', {path: '/'} );
+  this.route('world', {path: '/world'});
   this.route('selected', {path: '/selected'});
   this.route('local', {path: '/local'});
-  this.route('home', {path: '/test'} );
+  this.route('home', {path: '/home'} );
 });
