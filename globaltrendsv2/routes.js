3b2c9f3bb1bec64aff98941831cf5be6898417d8
rscanlon <rscanlon@wellesley.edu>
2015-01-19 15:16:59 -0500

    back tov1

--- globaltrendsv2/routes.js
+++
@@ -1,7 +0,0 @@
-Router.map(function(){
-  this.route('map', {path: '/'} );
-  this.route('world', {path: '/world'});
-  this.route('selected', {path: '/selected'});
-  this.route('local', {path: '/local'});
-  this.route('home', {path: '/home'} );
-});
