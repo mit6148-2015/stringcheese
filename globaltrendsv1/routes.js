de5681e50dd231cc6f54abb34d916730a0d07482
rscanlon <rscanlon@wellesley.edu>
2015-01-21 15:05:14 -0500

    tyring to fix hashtag showing up

--- globaltrendsv1/routes.js
+++ globaltrendsv1/routes.js
@@ -8,25 +8,53 @@ Router.route('/country/:country', function() {
 		  return country;
 		},
 		specificHashtag: function(){
-			// setTimeout(returnResult, 1000);
-		 //  var r;
-		 //  console.log("I'm here: " +JSON.stringify(country));
-		 //  Meteor.apply("getHashtagName", [JSON.parse(country)], [true], function(err, result){
-	  //     console.log("Error: " + err);
-	  //     console.log("Result(Hashtag from Country): "+ result);
-	  //     r = result;
-	  //     console.log("r before: "+ r);
-	  //     }); 
-	      
-	  //    returnResult(r);
-	  //    console.log("r: "+ r);
-	  return "Top Hashtag Will Be Here";
+			return wrappedGetHashtag(country);
+		//	 setTimeout(returnResult, 3000);
+		//   var r = "";
+		  // console.log("I'm here: " +JSON.stringify(country));
+		  // Meteor.apply("getHashtagName", [country], [true], function(err, result){
+	   //    console.log("Error: " + err);
+	   //    console.log("Result(Hashtag from Country): "+ result);
+	   // //   r = result;
+	   //    console.log("r before: "+ result);
+	   //    return result;
+	   //    }); 
+
+	     //  while(r="")
+	     //   console.log("r: "+ r);
+	     // var temp = returnResult(r);
+	     // return temp;
+	  
 	    }
 	  }
     }
   });
 	
 });
+
+function getUserProfile(req, callback) {
+  ghapi.user.getFrom(req, callback);
+}
+var wrappedGetProfile = Meteor._wrapAsync(getUserProfile);
+
+Meteor.methods({
+  getProfile: function(username) {
+    return wrappedGetProfile({user: username});
+  }
+});
+
+function getHashtagFromDB(country){
+	var r;
+	Meteor.apply("getHashtagName", [country], [true], function(err, result){
+	      console.log("Error: " + err);
+	      console.log("Result(Hashtag from Country): "+ result);
+	      r = result;
+	});
+	return r;
+}
+
+var wrappedGetHashtag = Meteor._wrapAsync(getHashtagFromDB);
+
 
 Router.map(function(){
   this.route('map', {path: '/'} );
