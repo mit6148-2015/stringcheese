30c656d1132d7880067bf6a75ec169f4929e49f6
rscanlon <rscanlon@wellesley.edu>
2015-01-24 12:07:44 -0500

    fixed not found url pages

--- globaltrendsv1/lib/routes.js
+++ globaltrendsv1/lib/routes.js
@@ -1,6 +1,27 @@
 Router.route('/country/:country', function() {
 	var country = this.params.country;
 
+
+
+	Meteor.call("getAllCountries", function(err, result){
+      var temp = JSON.parse(result);
+
+      var inDatabase = false;
+
+      for(var i=0; i<13;i++){
+        if(country===temp[i]){
+          inDatabase = true;
+        }
+      }
+
+      if(!inDatabase){
+		window.location.assign("/notFound");
+		return;
+      }
+    });
+
+
+
 	Meteor.apply("getHashtagName", [country], true, function(err, result){
 		      console.log("Error: " + err);
 		      console.log("Result(Hashtag from Country): "+ result);
@@ -31,13 +52,21 @@ Router.route('/country/:country', function() {
 	});
 	
 });
-
+Router.configure({
+//  layoutTemplate: 'layout',
+//  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  // waitOn: function(){
+  //   return Meteor.subscribe('userData');
+  // }
+});
 Router.map(function(){
   this.route('map', {path: '/'} );
   this.route('world', {path: '/world'});
   this.route('selected', {path: '/selected'});
   this.route('local', {path: '/local'});
   this.route('home', {path: '/home'} );
- //this.route('United States', {path: '/US'});
+  this.route('notFound', {path: '/notFound'});
+ // this.route('notFound', {path: '*' });
 });
 
