168cc9fa60f39760e82f899ba8c5288712891360
rscanlon <rscanlon@wellesley.edu>
2015-01-23 21:06:30 -0500

    haven't changed anything but want to pull new version

--- globaltrendsv1/client/instag.js
+++ globaltrendsv1/client/instag.js
@@ -1,15 +1,4 @@
 
-function getHashtagWithNoHashtag(){
-	var template = Template.instance();
-
-  var temp = template.$("#countryHashtag").text();
-  console.log("initial temp: "+ temp);
-  if(temp.charAt(0) ==='"'){
-  	temp = temp.substring(1, temp.length);
-  	console.log("temp: " + temp);
-  }
-  return temp;
-}
 var feed = new Instafeed({
   clientId: '356506ebf9c442bea6bd28d6a368ba9a',
   //tags
