b7250625f0aa9370b50085fbcf59b6846cc86001
rscanlon <rscanlon@wellesley.edu>
2015-02-18 16:40:59 -0500

    changed access keys so that Twitter works

--- globaltrendsv1/server/main.js
+++ globaltrendsv1/server/main.js
@@ -69,15 +69,15 @@ Meteor.methods({
  connectTwitter1: function() {
 
     var twit = new TwitMaker({
-        consumer_key: 'ES5GaX3Hd004VOryTvj6dBvxJ',
-        consumer_secret: '2wmBCpzUslGrB9cOE1XSfneJR9Uw4q8LdTz56hMSug3AiobD7T',
-        access_token: '2967109342-5juDANOJFAd1Ib9MzjRwOruLnHW3Lv4agitJaPC',
-        access_token_secret: 'qErlkWRnvM2diQkmg1SlBzOF1IINy1uZxDfvc6kVm12TF'
+        // consumer_key: 'ES5GaX3Hd004VOryTvj6dBvxJ',
+        // consumer_secret: '2wmBCpzUslGrB9cOE1XSfneJR9Uw4q8LdTz56hMSug3AiobD7T',
+        // access_token: '2967109342-5juDANOJFAd1Ib9MzjRwOruLnHW3Lv4agitJaPC',
+        // access_token_secret: 'qErlkWRnvM2diQkmg1SlBzOF1IINy1uZxDfvc6kVm12TF'
 
-//         consumer_key: '0PvZ5l8rtE36uokYn63f7NH6B',
-//         consumer_secret: 'onFHlgJ5Om6jVaGd1Bwdg234X8smxD2OQTDJg2RIjU7OAmGIfD',
-//         access_token: '2981621259-gQLus7QkNolFxs6HtedqPEDv8wopAGwgTREwAsN',
-//         access_token_secret: 'cGqjr7bfbkQ5exMw0X0OG68wkeKhMzhibSoZa7EEphoCP'
+        consumer_key: '0PvZ5l8rtE36uokYn63f7NH6B',
+        consumer_secret: 'onFHlgJ5Om6jVaGd1Bwdg234X8smxD2OQTDJg2RIjU7OAmGIfD',
+        access_token: '2981621259-gQLus7QkNolFxs6HtedqPEDv8wopAGwgTREwAsN',
+        access_token_secret: 'cGqjr7bfbkQ5exMw0X0OG68wkeKhMzhibSoZa7EEphoCP'
 
 
     });
@@ -116,10 +116,15 @@ Meteor.methods({
 //         access_token: '2981621259-gQLus7QkNolFxs6HtedqPEDv8wopAGwgTREwAsN',
 //         access_token_secret: 'cGqjr7bfbkQ5exMw0X0OG68wkeKhMzhibSoZa7EEphoCP'
 
-        consumer_key: 'V6pV7WkzMJDPT2smxEAJGJQXM',
-        consumer_secret: 'AEu10XYgK3RftfRvDGdwVnlbfxEuAgOWewcqeCt4uFZKSbyakQ',
-        access_token: '2966838142-M97BaLp0xIIeg1oNoxuuDGWm9k3N6J5xyOZh0Em',
-        access_token_secret: 'AifnQUfQRVQmfKluUFCim36iQDh28uQgB3St7HlBidGx4'
+        // consumer_key: 'V6pV7WkzMJDPT2smxEAJGJQXM',
+        // consumer_secret: 'AEu10XYgK3RftfRvDGdwVnlbfxEuAgOWewcqeCt4uFZKSbyakQ',
+        // access_token: '2966838142-M97BaLp0xIIeg1oNoxuuDGWm9k3N6J5xyOZh0Em',
+        // access_token_secret: 'AifnQUfQRVQmfKluUFCim36iQDh28uQgB3St7HlBidGx4'
+
+         consumer_key: 'BWM08c5Ig3FZj8EByFF5HjakM',
+         consumer_secret: 'FjNU5gDe8EicYRVcNpWQUTnEdACOq6jGHxlomO0WpjftfEBCmj',
+         access_token: '2967109342-Gm2Iahz8M2YW5Te55iXaO8fiti6TLuXmnwDVGCE',
+         access_token_secret: 'BFTigqMtuDi7WR64dl6xh49RRUed8fyfiInC9uKvacxAd'
     });
 
       countryIDList2.forEach(function(countryID) {
